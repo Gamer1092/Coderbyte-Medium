38cbc235d8c79915b7cd47dd34d668f52a0ecc90
Gamer1092 <lynjd@yahoo.ca>
2015-12-04 01:25:03 -0500

    Create Arith Geo II.cpp
    Determine if the elements in the array follow an arithmetic or geometric sequence.

---
+++ Arith Geo II.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+string ArithGeoII(int arr[]) { 
+  
+  // code goes here
+  int size = sizeof(arr)/sizeof(arr[0]);
+  int difference = arr[1] - arr[0];
+  int ratio = arr[1]/arr[0];
+  bool arithmetic = false;
+  bool geometric = false;
+  
+  for(int i = 1; i < size; i++){
+    if(arr[i+1] - arr[i] == difference){
+      arithmetic = true;
+    }
+    
+    else if(arr[i+1]/arr[i] == ratio){
+      geometric = true;
+    }
+  }
+  
+  if(arithmetic){
+   return "Arithmetic";
+  }
+  else if(geometric){
+    return "Geometric";
+  }
+  else{
+    return "-1";
+  }
+  return "-1"; 
+            
+}
+
+int main() { 
+   
+  // keep this function call here
+  /* Note: In C++ you first have to initialize an array and set 
+     it equal to the stdin to test your code with arrays. 
+     To see how to enter arrays as arguments in C++ scroll down */
+     
+  int A[] = gets(stdin);
+  cout << ArithGeoII(A);
+  return 0;
+    
+}           
